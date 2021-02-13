d8de0f3f0a76c3da5e0eba70ebb62454ba240779
= <stevenabaco@gmail.com>
2021-02-13 14:43:13 -0600

    Fix code for 404 render error

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
-const http = require("http");
 const path = require("path");
 
 const express = require("express");
-const app = express();
-
-app.set('view engine', 'ejs');
-app.set('views', 'views')
-
-const adminData = require("./routes/admin")
-const shopRoutes = require("./routes/shop");
-
 const bodyParser = require("body-parser");
 
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminData.routes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, '.', 'views', '404.html'));
-})
+    res.status(404).render("404", {
+        pageTitle: "Page Not Found",
+        path: ''
+    });
+});
 
 app.listen(3000);
