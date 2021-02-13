5876cb9c1fb06da68d8cac1d00e6ca7fc5c03e89
= <stevenabaco@gmail.com>
2021-02-13 14:47:52 -0600

    Add Controllers folder and start adding controllers for products

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,7 @@
 const path = require("path");
 
+const productsController = require('../controllers/products');
+
 const express = require("express");
 
 const router = express.Router();
@@ -7,15 +9,7 @@ const router = express.Router();
 const products = [];
 
 // /admin/add-product => GET
-router.get("/add-product", (req, res, next) => {
-	res.render("add-product", {
-		pageTitle: "Add Product",
-		path: "/admin/add-product",
-		formsCSS: true,
-		productCSS: true,
-		activeAddProduct: true,
-	});
-});
+router.get("/add-product", productsController.getAddProduct);
 
 router.post("/add-product", (req, res, next) => {
 	products.push({ title: req.body.title });
