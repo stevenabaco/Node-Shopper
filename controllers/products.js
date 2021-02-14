cb6039ffd1dfc034451d8e21693ca9d63437c602
= <stevenabaco@gmail.com>
2021-02-14 09:50:52 -0600

    Restructure Folder Layout to prep for creating the online shop experience

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
-const Product = require('../models/product')
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-	res.render("add-product", {
+	res.render("admin/add-product", {
 		pageTitle: "Add Product",
 		path: "/admin/add-product",
 		formsCSS: true,
@@ -11,21 +11,20 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title);
-    product.save();
+	const product = new Product(req.body.title);
+	product.save();
 	res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll((products) => {
-       res.render("shop", {
-		prods: products,
-		pageTitle: "Shop",
-		path: "/",
-		hasProducts: products.length > 0,
-		activeShop: true,
-		productCSS: true,
+	Product.fetchAll(products => {
+		res.render("shop/product-list", {
+			prods: products,
+			pageTitle: "Shop",
+			path: "/",
+			hasProducts: products.length > 0,
+			activeShop: true,
+			productCSS: true,
+		});
 	});
-    }); 
-}
-	
+};
