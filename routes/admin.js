2f1f26ebe0e4339b5da859289dd968f075c6b1a6
= <stevenabaco@gmail.com>
2021-02-14 11:25:39 -0600

    Register and link routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,18 @@
 const path = require("path");
 
-const productsController = require("../controllers/products");
+const adminController = require("../controllers/admin");
 
 const express = require("express");
 
 const router = express.Router();
 
-
-
 // /admin/add-product => GET
-router.get("/add-product", productsController.getAddProduct);
+router.get("/add-product", adminController.getAddProduct);
 
-router.post("/add-product", productsController.postAddProduct);
+// /admin/products => GET
+router.get("/products", adminController.getAddProduct);
+
+// /admin/add-product => POST
+router.post("/add-product", adminController.postAddProduct);
 
 module.exports = router;
