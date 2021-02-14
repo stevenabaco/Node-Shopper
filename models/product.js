19404e6c57768a81b6480508e18e817f4a183f9e
= <stevenabaco@gmail.com>
2021-02-14 16:02:24 -0600

    Add Changes for Mobile Devices and ID to the Path

--- models/product.js
+++ models/product.js
@@ -25,6 +25,7 @@ const getProductsFromFile = (cb) => {
         this.price = price;
     }
         save() {
+            this.id = Math.random().toString();
             getProductsFromFile(products => {
                 products.push(this);
                 fs.writeFile(p, JSON.stringify(products), err => {
