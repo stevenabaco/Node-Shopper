e0c441832c3dd2eb3d61b30475530664643fbc24
= <stevenabaco@gmail.com>
2021-02-14 13:08:04 -0600

    Add logic to store and display newly added items

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,18 +4,15 @@ exports.getAddProduct = (req, res, next) => {
 	res.render("admin/add-product", {
 		pageTitle: "Add Product",
 		path: "/admin/add-product",
-		formsCSS: true,
-		productCSS: true,
-		activeAddProduct: true,
 	});
 };
 
 exports.postAddProduct = (req, res, next) => {
     const title = req.body.title;
-    const image = req.body.imageUrl;
+    const image = req.body.imageUrl ;
     const price = req.body.price;
     const description = req.body.description;
-	const product = new Product(title, imageUrl, price, description);
+	const product = new Product(title, image, price, description);
 	product.save();
 	res.redirect("/");
 };
--- models/product.js
+++ models/product.js
@@ -18,7 +18,7 @@ const getProductsFromFile = (cb) => {
     });
 }
     module.exports = class Product {
-    constructor(title, imageUrl, description, price) {
+    constructor(title, imageUrl, price, description) {
         this.title = title;
         this.imageUrl = imageUrl;
         this.description = description;
