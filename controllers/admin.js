b9a8a75fa0770e2a104d8c46e1f196e5cbdd877a
= <stevenabaco@gmail.com>
2021-02-15 12:11:09 -0600

    Add logic to pre-populate the edit product page with data

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,29 +3,37 @@ const Product = require("../models/product");
 exports.getAddProduct = (req, res, next) => {
 	res.render("admin/edit-product", {
 		pageTitle: "Add Product",
-		path: "/admin/add-product",
+        path: "/admin/add-product",
+        editing: false
 	});
 };
 
 exports.postAddProduct = (req, res, next) => {
-    const title = req.body.title;
-    const image = req.body.imageUrl ;
-    const price = req.body.price;
-    const description = req.body.description;
+	const title = req.body.title;
+	const image = req.body.imageUrl;
+	const price = req.body.price;
+	const description = req.body.description;
 	const product = new Product(title, image, price, description);
 	product.save();
 	res.redirect("/");
 };
 
 exports.getEditProduct = (req, res, next) => {
-    const editMode = req.query.edit;
-    if (!editMode) {
-       return res.redirect('/');
-    }
-	res.render("admin/edit-product", {
-		pageTitle: "Edit Product",
-        path: "/admin/edit-product",
-        editing: editMode
+	const editMode = req.query.edit;
+	if (!editMode) {
+		return res.redirect("/");
+	}
+	const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+        if (!product) {
+            return res.redirect('/');
+        }
+		res.render("admin/edit-product", {
+			pageTitle: "Edit Product",
+			path: "/admin/edit-product",
+            editing: editMode,
+            product: product
+		});
 	});
 };
 
