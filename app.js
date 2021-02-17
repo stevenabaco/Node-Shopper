4aebf7283491243b61123bd487b95694eef20db6
= <stevenabaco@gmail.com>
2021-02-17 13:12:05 -0600

    Add a One-To-Many Relationship for Users to Products

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const sequelize = require('./util/database');
+const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 const app = express();
 
@@ -22,11 +24,15 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync().then(result => {
-    // console.log(result);
-    app.listen(3000)
-})
-    .catch(err => {
-        console.log(err);
-    });
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
 
+sequelize
+	.sync({ force: true })
+	.then(result => {
+		// console.log(result);
+		app.listen(3000);
+	})
+	.catch(err => {
+		console.log(err);
+	});
