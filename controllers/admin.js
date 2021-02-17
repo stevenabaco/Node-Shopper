7ede33f1792f213407017f9072c15aa7ab68f9bd
= <stevenabaco@gmail.com>
2021-02-17 16:27:29 -0600

    Add Magic Association to User for AddProduct

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,17 +13,20 @@ exports.postAddProduct = (req, res, next) => {
 	const imageUrl = req.body.imageUrl;
 	const price = req.body.price;
 	const description = req.body.description;
-	Product.create({
-		title: title,
-		price: price,
-		imageUrl: imageUrl,
-		description: description,
-	}).then(result => {
-		console.log(result);
-		res.redirect('/admin/products')
-	}).catch(err => {
-		console.log(err);
-	})
+	req.user
+		.createProduct({
+			title: title,
+			price: price,
+			imageUrl: imageUrl,
+			description: description,
+		})
+		.then(result => {
+			console.log(result);
+			res.redirect("/admin/products");
+		})
+		.catch(err => {
+			console.log(err);
+		});
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -34,17 +37,19 @@ exports.getEditProduct = (req, res, next) => {
 	const prodId = req.params.productId;
 	Product.findByPk(prodId)
 		.then(product => {
-		if (!product) {
-			return res.redirect("/");
-		}
-		res.render("admin/edit-product", {
-			pageTitle: "Edit Product",
-			path: "/admin/edit-product",
-			editing: editMode,
-			product: product,
+			if (!product) {
+				return res.redirect("/");
+			}
+			res.render("admin/edit-product", {
+				pageTitle: "Edit Product",
+				path: "/admin/edit-product",
+				editing: editMode,
+				product: product,
+			});
+		})
+		.catch(err => {
+			console.log(err);
 		});
-	})
-		.catch(err=>{console.log(err);})
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -62,32 +67,39 @@ exports.postEditProduct = (req, res, next) => {
 			return product.save();
 		})
 		.then(result => {
-			console.log('UPDATED PRODUCT!');
-			res.redirect('/admin/products');
+			console.log("UPDATED PRODUCT!");
+			res.redirect("/admin/products");
 		})
-		.catch(err => { console.log(err); })
+		.catch(err => {
+			console.log(err);
+		});
 };
 
 exports.getProducts = (req, res, next) => {
 	Product.findAll()
 		.then(products => {
-		res.render("admin/products", {
-			prods: products,
-			pageTitle: "Admin Products",
-			path: "/admin/products",
+			res.render("admin/products", {
+				prods: products,
+				pageTitle: "Admin Products",
+				path: "/admin/products",
+			});
 		})
-	}).catch(err=>{console.log(err)});
+		.catch(err => {
+			console.log(err);
+		});
 };
 
 exports.postDeleteProduct = (req, res, next) => {
 	const prodId = req.body.productId;
 	Product.findByPk(prodId)
 		.then(product => {
-			return product.destroy()
+			return product.destroy();
 		})
 		.then(result => {
-			console.log('DESTROYED PRODUCT');
-			res.redirect('/admin/products');
+			console.log("DESTROYED PRODUCT");
+			res.redirect("/admin/products");
 		})
-		.catch(err=>{console.log(err);});
-}
+		.catch(err => {
+			console.log(err);
+		});
+};
