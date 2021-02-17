dbbce9586a6f28029387919a162ff3b1c4e781bd
= <stevenabaco@gmail.com>
2021-02-16 21:22:43 -0600

    Add logic to Insert Data and Create new products using Add Product.

--- controllers/admin.js
+++ controllers/admin.js
@@ -10,16 +10,19 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
 	const title = req.body.title;
-	const image = req.body.imageUrl;
+	const imageUrl = req.body.imageUrl;
 	const price = req.body.price;
 	const description = req.body.description;
-	const product = new Product(null, title, image, description, price);
-	product
-		.save()
-		.then(() => {
-			res.redirect('/');
-		})
-		.catch(err => console.log(err));
+	Product.create({
+		title: title,
+		price: price,
+		imageUrl: imageUrl,
+		description: description,
+	}).then(result => {
+		console.log(result);
+	}).catch(err => {
+		console.log(err);
+	})
 };
 
 exports.getEditProduct = (req, res, next) => {
