94808689eadcc9fb29ecf17df67f510a3ac0bb17
= <stevenabaco@gmail.com>
2021-02-19 20:33:19 -0600

    Final Version with Sequelize prior to converting to MONGODB!

--- routes/shop.js
+++ routes/shop.js
@@ -13,7 +13,7 @@ router.get("/cart", shopController.getCart);
 router.post("/cart", shopController.postCart);
 router.post("/cart-delete-item", shopController.postCartDeleteProduct);
 router.get("/orders", shopController.getOrders);
-router.get("/checkout", shopController.getCheckout);
+// router.get("/checkout", shopController.getCheckout);
 router.post("/create-order", shopController.postOrder);
 
 module.exports = router;
