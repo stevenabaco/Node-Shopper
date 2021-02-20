5de426af101083f650837b667fde62fe802cf330
= <stevenabaco@gmail.com>
2021-02-19 22:00:52 -0600

    Connect new Database

--- util/database.js
+++ util/database.js
@@ -1,15 +1,29 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (cb) => {
 	MongoClient.connect(
-		"mongodb+srv://Wizard:yeas4zaix.nerk9NON@cluster0.nzoh4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+		"mongodb+srv://Wizard:yeas4zaix.nerk9NON@cluster0.p7gsm.mongodb.net/shop?retryWrites=true&w=majority"
 	)
 		.then(client => {
 			console.log("Database Connected!");
-			cb(client)
+			_db = client.db();
+			cb();
 		})
-		.catch(err => console.log(err));
+		.catch(err => {
+			console.log(err);
+			throw err;
+		});
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+	if (_db) {
+		return _db
+	}
+	throw 'No Database Found!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb
