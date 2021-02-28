21ba6bc9eb7c9e18a00137bb5461ae30ea09a885
= <stevenabaco@gmail.com>
2021-02-28 10:15:00 -0600

    Add signup email validation with error message

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
 
@@ -7,7 +8,7 @@ const router = express.Router();
 router.get('/login', authController.getLogin);
 router.get("/signup", authController.getSignup);
 router.post('/login', authController.postLogin);
-router.post("/signup", authController.postSignup);
+router.post("/signup", check('email').isEmail().withMessage('Please enter a valid email'),authController.postSignup);
 router.post('/logout', authController.postLogout);
 router.get('/reset', authController.getReset);
 router.post('/reset', authController.postReset);
