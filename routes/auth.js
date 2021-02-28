9379a0b9fa673f48bb155b26333b6f6c6d14488c
= <stevenabaco@gmail.com>
2021-02-28 11:11:38 -0600

    Add password Validations

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,34 @@
 const express = require('express');
-const { check } = require('express-validator/check');
+const { check, body } = require('express-validator');
 
 const authController = require('../controllers/auth');
 
 const router = express.Router();
 
 router.get('/login', authController.getLogin);
-router.get("/signup", authController.getSignup);
+router.get('/signup', authController.getSignup);
 router.post('/login', authController.postLogin);
-router.post("/signup", check('email').isEmail().withMessage('Please enter a valid email'),authController.postSignup);
+router.post(
+	'/signup',
+	[
+		check('email')
+			.isEmail()
+			.withMessage('Please enter a valid email')
+			.custom((value, { req }) => {
+				if (value === 'test@test.com') {
+					throw new Error('This email address is forbidden!');
+				}
+				return true;
+			}),
+		body(
+			'password',
+			'Please enter a password with only numbers and text and at least 4 characters.'
+		)
+			.isLength({ min: 4 })
+			.isAlphanumeric()
+	],
+	authController.postSignup
+);
 router.post('/logout', authController.postLogout);
 router.get('/reset', authController.getReset);
 router.post('/reset', authController.postReset);
