17f7065e09e61b9ee4b42ad136dae30b178aeaf7
= <stevenabaco@gmail.com>
2021-03-07 20:17:25 -0600

    adjust number of products

--- controllers/shop.js
+++ controllers/shop.js
@@ -8,7 +8,7 @@ const Product = require('../models/product');
 const Order = require('../models/order');
 const { runInNewContext } = require('vm');
 
-const ITEMS_PER_PAGE = 4;
+const ITEMS_PER_PAGE = 2;
 
 exports.getProducts = (req, res, next) => {
 	const page = +req.query.page || 1;
