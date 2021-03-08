eb4172e12f5916fcb9c45df1aecc4d7817ac4106
= <stevenabaco@gmail.com>
2021-03-08 13:09:49 -0600

    Fix app to work on shared hosting

--- app.js
+++ app.js
@@ -55,13 +55,13 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
-const accessLogStream = fs.createWriteStream(
-	path.join(__dirname, 'access.log'),
-	{ flags: 'a' }
-);
+// const accessLogStream = fs.createWriteStream(
+// 	path.join(__dirname, 'access.log'),
+// 	{ flags: 'a' }
+// );
 // app.use(helmet());
 app.use(compression());
-app.use(morgan('combined', { stream: accessLogStream }));
+// app.use(morgan('combined', { stream: accessLogStream }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
